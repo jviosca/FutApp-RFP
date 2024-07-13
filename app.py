c35fae231202a2f8fe2dcbf401dbab612d2d5a43
Jose Viosca Ros <jviosca@gmail.com>
2024-07-13 11:16:52 +0200

    Testear conexión con GSheets

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.write("Hello world")
 st.write("Pronto aquí estará la clasificación de Real Futbal Patata")
+
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
