7601c0744ad06ee0c2f60ca8080213cb8818f9d8
Jose Viosca Ros <jviosca@gmail.com>
2024-07-13 11:37:03 +0200

    Testear conexión con GSheets, mostrar dataframe

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(row)
+df = conn.read(worksheet="registro_partidos",
+                ttl="10m",
+                nrows="5")
+# show dataframe
+st.dataframe(df)
