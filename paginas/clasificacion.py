38aedca56f06d52eabdf61d05f44fa4a57e696c4
Jose Viosca Ros <jviosca@gmail.com>
2024-07-30 07:48:02 +0200

    Cambiar fecha umbral para mostrar cartel de clasificacion transitoria eliminando filas de registros_partidos donde jugador es NaN

--- paginas/clasificacion.py
+++ paginas/clasificacion.py
@@ -14,8 +14,9 @@ mvp_temporada_elegida = mvp.loc[mvp['temporada_n']==temporada_elegida_n]
 partidos_temporada_elegida = partidos.loc[partidos['temporada']==temporada_elegida_n]
 #st.dataframe(partidos_temporada_elegida)
 if partidos_temporada_elegida.shape[0] > 0:
-    fecha_ultimo_partido = datetime.datetime.strptime(partidos.iloc[-1]['fecha'],'%d/%m/%Y')
+    fecha_ultimo_partido = datetime.datetime.strptime(partidos.dropna(subset='jugador').iloc[-1]['fecha'],'%d/%m/%Y')
     fecha_cierre_mvp = fecha_ultimo_partido + datetime.timedelta(days=3)
+    #st.write(fecha_ultimo_partido)
     if datetime.datetime.today() < fecha_cierre_mvp and datetime.datetime.today() > fecha_ultimo_partido:
         st.markdown("**Esta clasificación es temporal y está pendiente de la votación del MVP. La fecha límite para la votación es el jueves a las 23:59. A partir del viernes, la clasificación será definitiva**")
     clasificacion_df = aux.clasificacion(partidos_temporada_elegida,jugadores,mvp_temporada_elegida)
