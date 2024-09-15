eb8582123d33eb26bfa6c59166cd732d9ce4943a
Jose Viosca Ros <jviosca@gmail.com>
2024-09-15 10:44:30 +0200

    Añade ayuda y detalles a pestaña Partidos de página Admin

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,6 +1,7 @@
 import hmac
 import streamlit as st
 import sys
+import pandas as pd
 
 
 def check_password():
@@ -65,26 +66,64 @@ with tab1:
         col2a,col2b = st.columns(2)
         with col2a:
             st.header("Equipo 1")
-            equipo_A_goles_metidos_seleccion = st.selectbox("Goles a favor", list(range(0,20)), index=int(equipo_A_goles_metidos))
-            equipo_A_goles_recibidos_seleccion = st.selectbox("Goles en contra", list(range(0,20)), index=int(equipo_A_goles_recibidos))
+            equipo_A_goles_metidos_seleccion = st.selectbox("Goles a favor", 
+                    list(range(0,20)), 
+                    index=int(equipo_A_goles_metidos),
+                    help='''Goles totales metidos por el equipo 1. 
+                    Si juegan jugadores no registrados, 
+                    puede no coincidir con la suma de goles de abajo''')
+            equipo_A_goles_recibidos_seleccion = st.selectbox("Goles en contra", 
+                    list(range(0,20)), 
+                    index=int(equipo_A_goles_recibidos),
+                    help="Goles totales recibidos por el equipo 1")
+            st.divider()
+            st.write("Jugadores registrados y goles metidos.")
+            st.write("En la columna de la izquierda, añade los jugadores apuntados. A la derecha, los goles.")
+            # si el dataframe no tiene 7 filas, añadimos hasta 7
+            while equipo_A.shape[0] < 7:
+                equipo_A = pd.concat([equipo_A,pd.DataFrame(data = {'jugador':[None], 'goles_metidos': [0]})])                 
             st.data_editor(equipo_A[['jugador','goles_metidos']], hide_index=True, use_container_width=True,
                             column_config ={
                                 "jugador": st.column_config.SelectboxColumn(
                                     "Jugador",
-                                    options=st.session_state.jugadores['nombre'].sort_values(key=lambda col: col.str.lower())),
-                                "goles_metidos":st.column_config.NumberColumn("Goles", default=0, min_value=0, max_value=20, required=True, step='int')
+                                    options=st.session_state.jugadores['nombre'].sort_values(key=lambda col: col.str.lower()),
+                                    help='''Haz click sobre una fila y selecciona en 
+                                    el desplegable el jugador. Puedes buscar introduciendo texto.'''),
+                                "goles_metidos":st.column_config.NumberColumn("Goles", 
+                                    default=0, min_value=0, max_value=20, 
+                                    required=True, step='int',
+                                    help="Introduce un número entero entre 0 y 20")
                                     })
             st.write("Suma de goles = " + str(int(equipo_A['goles_metidos'].sum())))
         with col2b:
             st.header("Equipo 2")
-            equipo_B_goles_metidos_seleccion = st.selectbox("Goles a favor", list(range(0,20)), index=int(equipo_B_goles_metidos))
-            equipo_B_goles_recibidos_seleccion = st.selectbox("Goles en contra", list(range(0,20)), index=int(equipo_B_goles_recibidos))
+            equipo_B_goles_metidos_seleccion = st.selectbox("Goles a favor", 
+                    list(range(0,20)), 
+                    index=int(equipo_B_goles_metidos),
+                    help='''Goles totales metidos por el equipo 2. 
+                    Si juegan jugadores no registrados, 
+                    puede no coincidir con la suma de goles de abajo''')
+            equipo_B_goles_recibidos_seleccion = st.selectbox("Goles en contra", 
+                    list(range(0,20)), 
+                    index=int(equipo_B_goles_recibidos),
+                    help="Goles totales recibidos por el equipo 2")
+            st.divider()
+            st.write("Jugadores registrados y goles metidos.")
+            st.write("En la columna de la izquierda, añade los jugadores apuntados. A la derecha, los goles.")
+            # si el dataframe no tiene 7 filas, añadimos hasta 7
+            while equipo_B.shape[0] < 7:
+                equipo_B = pd.concat([equipo_B,pd.DataFrame(data = {'jugador':[None], 'goles_metidos': [0]})])    
             st.data_editor(equipo_B[['jugador','goles_metidos']], hide_index=True, use_container_width=True,
                             column_config ={
                                 "jugador": st.column_config.SelectboxColumn(
                                     "Jugador",
-                                    options=st.session_state.jugadores['nombre'].sort_values(key=lambda col: col.str.lower())),
-                                "goles_metidos":st.column_config.NumberColumn("Goles", default=0, min_value=0, max_value=20, required=True, step='int')
+                                    options=st.session_state.jugadores['nombre'].sort_values(key=lambda col: col.str.lower()),
+                                    help='''Haz click sobre una fila y selecciona en 
+                                    el desplegable el jugador. Puedes buscar introduciendo texto.'''),
+                                "goles_metidos":st.column_config.NumberColumn("Goles", 
+                                    default=0, min_value=0, max_value=20, 
+                                    required=True, step='int',
+                                    help="Introduce un número entero entre 0 y 20")
                                     })
             st.write("Suma de goles = " + str(int(equipo_B['goles_metidos'].sum())))
         submitted = st.form_submit_button("Guardar")
